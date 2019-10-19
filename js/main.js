7fd568b64645596be1325cb0a00e99733af0aae5
zacosta1 <31709598+zacosta1@users.noreply.github.com>
2019-10-19 16:47:39 -0600

    Upload files

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+$('.menu-btn').click(function(e)
+{
+
+	$('nav').toggle();
+	e.preventDefault();
+})
+
+// $(document).ready(function()
+// {
+// 	$(window).on('resize', function()
+// 	{
+		
+// 	});
+// });
